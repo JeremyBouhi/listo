cfe2f9150fb3540638e296ab6dc2efed06fd6b73
Jerem <jeremy.bouhi@edu.ece.fr>
2019-01-08 17:46:27 +0100

    node + express + angular + mongodb mais commenté

---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+angular.module('sampleApp', ['ngRoute', 'appRoutes', 'OverviewCtrl', 'TripCtrl', 'TripService']);
---
+++ public/js/controllers/overview.controller.js
@@ -0,0 +1,5 @@
+angular.module('OverviewCtrl', []).controller('OverviewController', function($scope) {
+
+    $scope.tagline = 'To the moon and back!';   
+
+});
