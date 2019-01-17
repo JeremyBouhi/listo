925338b7166c932b34d45608902e6226c1cc39c1
Jerem <jeremy.bouhi@edu.ece.fr>
2019-01-17 15:42:55 +0100

    cookie parser added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var app            = express();
 var session        = require('express-session');
 var MongoDBStore   = require('connect-mongodb-session')(session);
 var bodyParser     = require('body-parser');
+var cookieParser   = require('cookie-parser');
 var methodOverride = require('method-override');
 var mongoose       = require('mongoose');
 var cors           = require('cors')
@@ -25,22 +26,12 @@ store.on('error', function(error) {
     assert.ok(false);
 });
 
-// get all data/stuff of the body (POST) parameters
-// parse application/json
 app.use(bodyParser.json());
-
-// parse application/vnd.api+json as json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(methodOverride('X-HTTP-Method-Override'));
 
-// set the static files location /public/img will be /img for users
-app.use(express.static(__dirname + '/public'));
-
+app.use(cookieParser());
 app.use(cors());
 
 app.set('trust proxy', 1)
