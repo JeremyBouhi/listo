353a0c084b46046b703870323e393b5a3232ed7b
Jerem <jeremy.bouhi@edu.ece.fr>
2019-01-17 13:08:15 +0100

    changed Unintialized

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(session({
     secret:process.env.SESSION_SECRET,
     resave:false,
-    saveUninitialized:true,
+    saveUninitialized:false,
     maxAge: 1000 * 60 * 60 * 24,
     cookie: {
         secure: true
