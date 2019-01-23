8d816c1c7737b00b15fd209ce078fb393ea26f35
Jerem <jeremy.bouhi@edu.ece.fr>
2019-01-23 17:57:41 +0100

    saveBudget is modified : the mean budget is now automatically calculated

--- controllers/budget.js
+++ controllers/budget.js
@@ -3,15 +3,23 @@ import User from './../models/user'
 
 
 var budgetController = {
-    
+
     saveBudget: function(req,res) {
         
         if(!req.session.user){
             console.log("Problem when accessing information of user");
             res.status(401).send();
         }
+
+        var counterOfUsersWhoAlreadySetBudget = 0;
+        var meanBudgetTransportation = 0;
+        var meanBudgetAccommodation = 0;
+        var meanBudgetOnTheSpot = 0;
+        var meanBudgetTotal = 0;
+
         Trip.findOne({_id : req.params.tripId
         }).then((trip) => {
+            //to update budget to the current user
             trip.users.map((user) => {
                 if(user._id == req.session.user._id.toString()){
 
@@ -21,7 +29,29 @@ var budgetController = {
                     user.budget.total = req.body.total
                 }
             })
-            
+            //to calculate the mean budget of the whole group
+            trip.users.map((user) => {
+                //without taking them who didn't set their budget yet
+                if(user.budget.transportation!=0 && user.budget.transportation!=0 && user.budget.transportation!=0) {
+                    meanBudgetTransportation += user.budget.transportation;
+                    meanBudgetAccommodation += user.budget.accommodation;
+                    meanBudgetOnTheSpot += user.budget.on_the_spot;
+                    meanBudgetTotal += user.budget.total;
+                    counterOfUsersWhoAlreadySetBudget ++;
+                }
+            })
+
+            meanBudgetTransportation /= counterOfUsersWhoAlreadySetBudget;
+            meanBudgetAccommodation /= counterOfUsersWhoAlreadySetBudget;
+            meanBudgetOnTheSpot /= counterOfUsersWhoAlreadySetBudget;
+            meanBudgetTotal /= counterOfUsersWhoAlreadySetBudget;
+
+            //updating in DB
+            trip.budget.transportation = meanBudgetTransportation;
+            trip.budget.accommodation = meanBudgetAccommodation;
+            trip.budget.on_the_spot = meanBudgetOnTheSpot;
+            trip.budget.total = meanBudgetTotal;
+
             trip.save((err, result) => {
             if(err) {
                 console.log('err: ', err);
