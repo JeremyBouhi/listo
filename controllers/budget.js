839eb0bb42394104b1e91a519c17c94c95638620
Jerem <jeremy.bouhi@edu.ece.fr>
2019-01-24 14:58:30 +0100

    getBudget return meanBudget as well

--- controllers/budget.js
+++ controllers/budget.js
@@ -76,15 +76,17 @@ var budgetController = {
             var promise =  trip.users.map((user) => {
                 if(user._id == req.session.user._id.toString()){
 
-                    return user.budget
+                    return {
+                        meanBudget: trip.budget,
+                        myBudget: user.budget
+                    }
                 }
-            })          
+            })         
             
-            Promise.resolve(promise).then(function(budget) {
-                console.log('budget[0]: ', budget[0]);
-
+            Promise.resolve(promise).then(function(result) {
+                // console.log('budget[0]: ', budget[0]);
                 // [0] because of CoreMongooseArray...
-                res.status(200).send(budget[0])
+                res.status(200).send(result[0])
             })
     }).catch((err) => {
         res.status(500).send(err)
