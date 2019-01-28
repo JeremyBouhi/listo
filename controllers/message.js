fca923077ba48946fc1a5b6783e889aa745346c1
Masson <Pierre-Yves.Masson@avl.com>
2019-01-28 16:10:20 +0100

    change on list modify

--- controllers/message.js
+++ controllers/message.js
@@ -62,44 +62,52 @@ var messageController = {
     var iofile = require('../app.js');
     var io = iofile.io;
     var nsp = io.of('/chat');
+    
     nsp.on('connection', function(socket,pseudo){
     console.log('someone connected');
+    console.log(socket.id);
 
-      socket.on('nouveau_client', function(pseudo,tripId,topic) { 
+    socket.on('nouveau_client', function(pseudo,tripId,topic) { 
         socket.pseudo = pseudo;
         console.log("new client connected !");
         var room = tripId+"/"+topic;
         socket.join(room);
         socket.broadcast.to(room).emit('nouveau_client', pseudo);
-      });
+        console.log(pseudo);
+    });
   
       // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
-      socket.on('message', function (message,tripId,topic) {
-        message = ent.encode(message);
+    socket.on('message', function (message,tripId,topic) {
+        var messageReceived = ent.encode(message);
         var room = tripId+"/"+topic;
         socket.join(room);
-        socket.broadcast.to(room).emit('message',{pseudo:socket.pseudo,message:message});
+        socket.broadcast.to(room).emit('message',{pseudo:socket.pseudo,message:messageReceived});
         console.log("message envoyé à tout le monde");
         var datetime = new Date();//Retrieve time but with 1 hour less
         datetime.setTime(datetime.getTime() - new Date().getTimezoneOffset()*60*1000);//Set the correct time
-        var message=new Message({
+        console.log(req.session.user);
+        var messagedb=new Message({
             trip_id : req.params.tripId,
-            content : req.body.content,
+            content : messageReceived,
             sender : req.session.user._id,
             topic : req.params.topic,
             date : datetime
-         });
+            });
 
-         message.save((err, result) => {
+        messagedb.save((err, result) => {
             if(err) {
                 console.log("err : "+err);
                 console.log("There is an error in adding message in database");
                 res.status(500).send();
             }
             else res.status(200).send();
-        }) 
-        });
-  });
+            }) 
+    });
+
+    socket.on("disconnect",function(data){
+        socket.dosconnect();
+      });
+});
     }
 
 };
