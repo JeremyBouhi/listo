b007cb8a8f49d146832afcec38f3fac07836b05a
Jerem <jeremy.bouhi@edu.ece.fr>
2019-02-07 09:46:36 +0100

    cors http address added

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 app.use(cors({
-    origin:['http://localhost:4200'],
+    origin:['http://localhost:4200', 'https://listo-web.herokuapp.com'],
     credentials: true // enable set cookie
 }));
 
